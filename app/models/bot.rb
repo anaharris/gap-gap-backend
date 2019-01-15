d9a9ed98255708020b24e90afc16a3aa68bb7c38
Ana Harris <vana1990@gmail.com>
2019-01-14 20:06:28 -0500

    associations work

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,4 +1,5 @@
 class Bot < ApplicationRecord
+  has_many :bot_conversations
   has_many :conversations, through: :bot_conversations
 
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,6 @@
 class Conversation < ApplicationRecord
   has_many :user_conversations
+  has_many :bot_conversations
   has_many :bots, through: :bot_conversations
   has_many :users, through: :user_conversations
   has_many :messages, dependent: :destroy
