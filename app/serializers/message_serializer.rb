e7970192759f073986c337efea47c5f5c3561c1b
Ana Harris <vana1990@gmail.com>
2019-01-23 16:33:04 -0500

    changed serializer

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,4 +1,4 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :conversation_id, :user_id, :user_name, :content, :created_at
+  attributes :id, :conversation_id, :user_id, :bot_id, :user_name, :content, :avatar, :created_at
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :username, :avatar
   has_many :conversations
+
 end
