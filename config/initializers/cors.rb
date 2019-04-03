733c6a6d5db0b44d3b309ab4d507e221cc873d7b
Ana Harris <vana1990@gmail.com>
2019-04-02 22:15:32 -0400

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.env.production? ? 'http://www.anavharris.com' : '*'
+    origins Rails.env.production? ? 'http://gap-gap.anavharris.com' : '*'
 
     resource '*',
       headers: :any,
