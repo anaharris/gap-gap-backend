8bbbcaef864078ad46caba6d571e1973d5142cb3
Ana Harris <vana1990@gmail.com>
2019-04-02 22:35:49 -0400

    cable and cors again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://gap-gap.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://gap-gap.anavharris.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
